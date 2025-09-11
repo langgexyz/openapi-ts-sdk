32e65b2e3c7e968a40006bf06f947b7bf0ccc721
langgexyz <coderlang@foxmail.com>
2025-09-11 12:25:30 +0800

    refactor: merge http interfaces with abstract builder class
    - Merge http.interface.ts into http-builder.abstract.ts
    - Combine Http interface, HttpMethod enum and HttpBuilder abstract class in one file
    - Update core/index.ts to only export the unified http-builder.abstract module
    - Delete redundant http.interface.ts file
    
    This follows the established pattern of keeping related definitions together
    and reduces file fragmentation in the core module.

--- src/core/http-builder.abstract.ts
+++ src/core/http-builder.abstract.ts
@@ -1,4 +1,26 @@
-import { Http, HttpMethod } from './http.interface';
+/**
+ * HTTP 请求接口
+ */
+export interface Http {
+  /**
+   * 发送 HTTP 请求
+   * @returns Promise<[response: string, error: Error | null]>
+   */
+  send(): Promise<[string, Error | null]>;
+}
+
+/**
+ * HTTP 方法枚举
+ */
+export enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+  PATCH = 'PATCH',
+  HEAD = 'HEAD',
+  OPTIONS = 'OPTIONS'
+}
 
 /**
  * HTTP 构建器抽象类
